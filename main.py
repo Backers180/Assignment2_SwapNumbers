bacc906e6522bfd91422f3d4826b4e4031711eec
Scarabyte42 <alexander.green@ocsbstudent.ca>
2020-09-24 17:01:29 +0000

    Optimized code

--- main.py
+++ main.py
@@ -4,15 +4,9 @@ print("without using a temporary variable")
 x = int(input("Enter first number: "))
 y = int(input("Enter second number: "))
 
-if x > y:
-  x = x+y
-  y = x-y
-  x = x-y
-  print("Your first number is:", x)
-  print("Your second number is:", y)
-else:
-  y = y+x
-  x = y-x
-  y = y-x
-  print("Your first number is:", x)
-  print("Your second number is:", y)
+
+x = x+y
+y = x-y
+x = x-y
+print("Your first number is:", x)
+print("Your second number is:", y)
